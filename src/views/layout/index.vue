e6c5445ea005fe5b363d4b27e99fad759e85ade9
lencamo <207141481@qq.com>
2022-07-20 15:51:51 +0800

    实现layout组件底部标签栏跳转

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="layout-container">
     <!-- 头部区域 -->
-    <van-nav-bar title="标题" />
+    <van-nav-bar :title="activeTitle" />
 
     <!-- 内容区域 -->
     <router-view></router-view>
 
     <!-- 底部区域 -->
-    <van-tabbar v-model="active" route>
-      <van-tabbar-item icon="home-o" replace to="/layout/home"
+    <van-tabbar v-model="active" @change="onChange" route>
+      <van-tabbar-item icon="home-o" replace to="/layout/home" name="首页"
         >首页</van-tabbar-item
       >
-      <van-tabbar-item icon="search" replace to="/layout/search"
+      <van-tabbar-item icon="search" replace to="/layout/search" name="搜索"
         >搜索</van-tabbar-item
       >
     </van-tabbar>
@@ -22,10 +22,17 @@ export default {
   name: 'comLayout',
   data() {
     return {
-      active: 0
+      active: 0, // 默认选中的标签，当然也可以使用name值：active: '首页'
+      activeTitle: '首页'
     }
   },
-  methods: {}
+  methods: {
+    // 优化：不使用active改变title，改用@change事件间接改变title值
+    onChange(activeValue) {
+      // console.log(activeValue)
+      this.activeTitle = activeValue
+    }
+  }
 }
 </script>
 <style lang="less" scoped></style>
