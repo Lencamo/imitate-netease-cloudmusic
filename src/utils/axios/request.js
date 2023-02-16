ad8082a1defe8c03177162229cb490bca738663a
Mr.Ren <207141481@qq.com>
2023-02-16 22:03:49 +0800

    Update request.js

--- src/utils/axios/request.js
+++ src/utils/axios/request.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const reqAxios = axios.create({
-  baseURL: 'http://localhost:3000'
+//   baseURL: 'http://localhost:3000'
+  baseURL: 'https://netease-cloud-music-api-pink-pi.vercel.app/'
 })
 
 export default reqAxios
